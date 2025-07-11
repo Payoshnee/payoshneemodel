366a61c41bc026cfaef647162d5b5660949f19d5
Payoshnee Joshi <payoshneejoshi@gmail.com>
2025-07-11 22:07:51 +0530

    Revert "Add test Java file for AutoReviewBot"

--- bad_example.java
+++
@@ -1,7 +0,0 @@
-public class bad_example {
-    public void printAll(List<String> items) {
-        for (int i = 0; i < items.size(); i++) {
-            System.out.println(items.get(i));
-        }
-    }
-}
