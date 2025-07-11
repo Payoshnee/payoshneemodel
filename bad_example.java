8d73be09dc75af0897f71712a6cfcb5b13d84333
Payoshnee <payoshneejoshi@gmail.com>
2025-07-11 22:15:27 +0530

    javachange

--- bad_example.java
+++ bad_example.java
@@ -2,6 +2,8 @@ public class bad_example {
     public void printAll(List<String> items) {
         for (int i = 0; i < items.size(); i++) {
             System.out.println(items.get(i));
+            System.out.println(items.get(i));
+            System.out.println(items.get(i));
         }
     }
 }
