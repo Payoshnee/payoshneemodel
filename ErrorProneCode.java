faaed5071b23980f0542c5257b61b26dae208a95
Shark <payoshneejoshi@gmail.com>
2025-07-14 01:22:25 +0530

    commit_1

--- ErrorProneCode.java
+++ ErrorProneCode.java
@@ -16,7 +16,7 @@ public class ErrorProneCode {
 
         for (int i = 0; i <= 10; i++) {  
             if (i = 5) {  
-                break  
+                break.args 
             }
         }
 
